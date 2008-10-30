cb4f86b87bb28c2b64051082702957950838848e
Adam Jackson <ajax@redhat.com>
2008-10-30 16:33:21 -0400

    Initial skeleton driver

---
+++ src/qxl.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2008 Red Hat, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * on the rights to use, copy, modify, merge, publish, distribute, sub
+ * license, and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "config.h"
+
+#include "xf86.h"
+#include "xf86Resources.h"
+#include "xf86PciInfo.h"
+#include "xf86Cursor.h"
+#include "xf86_OSproc.h"
+#include "exa.h"
+#include "xf86xv.h"
+#ifdef XSERVER_PCIACCESS
+#include "pciaccess.h"
+#endif
+
+#define hidden _X_HIDDEN
+
+#define PCI_VENDOR_QUMRANET	0x1af4
+
+#define PCI_CHIP_QXL_0100	0x0100
+
+typedef struct _qxlScreen
+{
+    void *			reg;
+    void *			mem;
+
+    EntityInfoPtr		entity;
+
+#ifdef XSERVER_LIBPCIACCESS
+    struct pci_device *		pci;
+#else
+    pciVideoPtr			pci;
+    PCITAG			pciTag;
+#endif
+
+    CloseScreenProcPtr		CloseScreen;
+} qxlScreen;
---
+++ src/qxl_driver.c
@@ -0,0 +1,454 @@
+/*
+ * Copyright 2008 Red Hat, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * on the rights to use, copy, modify, merge, publish, distribute, sub
+ * license, and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/** \file qxl_driver.c
+ * \author Adam Jackson <ajax@redhat.com>
+ *
+ * This is qxl, a driver for the Qumranet paravirtualized graphics device
+ * in qemu.
+ */
+
+#include "qxl.h"
+
+#define qxlSaveState(x) do {} while (0)
+#define qxlRestoreState(x) do {} while (0)
+
+static Bool
+qxlBlankScreen(ScreenPtr pScreen, int mode)
+{
+    return TRUE;
+}
+
+static void
+qxlUnmapMem(qxlScreen *qxl, int scrnIndex)
+{
+#ifdef XSERVER_LIBPCIACCESS
+    if (qxl->mem)
+	pci_device_unmap_range(qxl->pci, qxl->mem, 0 /* size */);
+#else
+    if (qxl->mem)
+	xf86UnMapVidMem(scrnIndex, qxl->mem, 1 << qxl->pci->size[0]);
+    if (qxl->reg)
+	xf86UnMapVidMem(scrnIndex, qxl->reg, 8 * 1024);
+#endif
+    qxl->mem = qxl->reg = NULL;
+}
+
+static Bool
+qxlCloseScreen(int scrnIndex, ScreenPtr pScreen)
+{
+    ScrnInfoPtr pScrn = xf86Screens[scrnIndex];
+    qxlScreen *qxl = pScrn->driverPrivate;
+
+    if (pScrn->vtSema)
+	qxlUnmapMem(qxl, scrnIndex);
+#if 0 /* exa */
+    qxlExaFini(pScreen);
+#endif
+    pScrn->vtSema = FALSE;
+
+    pScreen->CloseScreen = qxl->CloseScreen;
+
+    return pScreen->CloseScreen(scrnIndex, pScreen);
+}
+
+static Bool
+qxlMapMemory(qxlScreen *qxl, int scrnIndex)
+{
+#ifdef XSERVER_LIBPCIACCESS
+    pci_device_map_range(qxl->pci, 0, 0, /* base, size */
+			 PCI_DEV_MAP_FLAG_WRITABLE |
+			    PCI_DEV_MAP_FLAG_WRITE_COMBINE,
+			 &qxl->mem);
+
+    if (!qxl->mem)
+	return FALSE;
+#else
+    qxl->mem = xf86MapPciMem(scrnIndex, VIDMEM_FRAMEBUFFER, qxl->pciTag,
+			     qxl->pci->memBase[0],
+			     (1 << qxl->pci->size[0]));
+    if (!qxl->mem)
+	return FALSE;
+
+    qxl->reg = xf86MapPciMem(scrnIndex, VIDMEM_MMIO | VIDMEM_MMIO_32BIT,
+			     qxl->pciTag, qxl->pci->memBase[1],
+			     8 * 1024);
+    if (!qxl->reg)
+	return FALSE;
+#endif
+}
+
+static Bool
+qxlScreenInit(int scrnIndex, ScreenPtr pScreen, int argc, char **argv)
+{
+    ScrnInfoPtr pScrn = xf86Screens[scrnIndex];
+    qxlScreen *qxl = pScrn->driverPrivate;
+
+    if (!qxlMapMemory(qxl, scrnIndex))
+	return FALSE;
+
+    qxlSaveState(qxl);
+    qxlBlankScreen(pScreen, SCREEN_SAVER_ON);
+    /* mode set */
+
+    miClearVisualTypes();
+    if (!miSetVisualTypes(pScrn->depth, miGetDefaultVisualMask(pScrn->depth),
+			  pScrn->rgbBits, pScrn->defaultVisual))
+	goto out;
+    if (!miSetPixmapDepths())
+	goto out;
+    if (!fbScreenInit(pScreen, qxl->mem, pScrn->virtualX, pScrn->virtualY,
+		      pScrn->xDpi, pScrn->yDpi, pScrn->displayWidth,
+		      pScrn->bitsPerPixel))
+	goto out;
+    fbPictureInit(pScreen, 0, 0);
+    {
+	VisualPtr visual = pScreen->visuals + pScreen->numVisuals;
+	while (--visual >= pScreen->visuals) {
+	    if ((visual->class | DynamicClass) == DirectColor) {
+                visual->offsetRed = pScrn->offset.red;
+                visual->offsetGreen = pScrn->offset.green;
+                visual->offsetBlue = pScrn->offset.blue;
+                visual->redMask = pScrn->mask.red;
+                visual->greenMask = pScrn->mask.green;
+                visual->blueMask = pScrn->mask.blue;
+	    }
+	}
+    }
+
+#if 0 /* EXA accel */
+    qxl->exa = qxlExaInit(pScreen);
+#endif
+
+    miDCInitialize(pScreen, xf86GetPointerScreenFuncs());
+
+#if 0 /* hardware cursor */
+    qxlCursorInit(pScreen);
+#endif
+
+    if (!miCreateDefColormap(pScreen))
+	goto out;
+
+    /* xf86DPMSInit(pScreen, xf86DPMSSet, 0); */
+
+#if 0 /* XV accel */
+    qxlInitVideo(pScreen);
+#endif
+
+    pScreen->SaveScreen = qxlBlankScreen;
+    qxl->CloseScreen = pScreen->CloseScreen;
+    pScreen->CloseScreen = qxlCloseScreen;
+
+    return TRUE;
+
+out:
+    return FALSE;
+}
+
+static Bool
+qxlEnterVT(int scrnIndex, int flags)
+{
+    qxlScreen *qxl = xf86Screens[scrnIndex]->driverPrivate;
+
+    qxlSaveState(qxl);
+
+    /* mode set */
+    return TRUE;
+}
+
+static void
+qxlLeaveVT(int scrnIndex, int flags)
+{
+    qxlScreen *qxl = xf86Screens[scrnIndex]->driverPrivate;
+
+    qxlRestoreState(qxl);
+}
+
+static Bool
+qxlColorSetup(ScrnInfoPtr pScrn)
+{
+    int scrnIndex = pScrn->scrnIndex;
+    rgb rzeros = { 0, 0, 0 };
+    Gamma gzeros = { 0.0, 0.0, 0.0 };
+
+    if (!xf86SetDepthBpp(pScrn, 0, 0, 0, Support32bppFb))
+	return FALSE;
+    if (pScrn->depth != 16 && pScrn->depth != 24) {
+	xf86DrvMsg(scrnIndex, X_ERROR, "Depth %d is not supported\n",
+		   pScrn->depth);
+	return FALSE;
+    }
+    xf86PrintDepthBpp(pScrn);
+
+    if (!xf86SetWeight(pScrn, rzeros, rzeros))
+	return FALSE;
+    if (!xf86SetDefaultVisual(pScrn, -1))
+	return FALSE;
+    if (!xf86SetGamma(pScrn, gzeros))
+	return FALSE;
+
+    return TRUE;
+}
+
+static Bool
+qxlPreInit(ScrnInfoPtr pScrn, int flags)
+{
+    int scrnIndex = pScrn->scrnIndex;
+    qxlScreen *qxl = NULL;
+    ClockRangePtr clockRanges = NULL;
+    int *linePitches = NULL;
+
+    /* zaphod mode is for suckers and i choose not to implement it */
+    if (xf86IsEntityShared(pScrn->entityList[0])) {
+	xf86DrvMsg(scrnIndex, X_ERROR, "No Zaphod mode for you\n");
+	return FALSE;
+    }
+
+    if (!pScrn->driverPrivate)
+	pScrn->driverPrivate = qxl = xnfcalloc(sizeof(qxlScreen), 1);
+    
+    qxl->entity = xf86GetEntityInfo(pScrn->entityList[0]);
+    qxl->pci = xf86GetPciInfoForEntity(qxl->entity->index);
+#ifndef XSERVER_LIBPCIACCESS
+    qxl->pciTag = pciTag(qxl->pci->bus, qxl->pci->device, qxl->pci->func);
+#endif
+
+    pScrn->monitor = pScrn->confScreen->monitor;
+
+    if (!qxlColorSetup(pScrn))
+	goto out;
+
+    /* option parsing and card differentiation */
+    xf86CollectOptions(pScrn, NULL);
+    
+    if (!qxlMapMemory(qxl, scrnIndex))
+	goto out;
+
+    /* ddc stuff here */
+
+    clockRanges = xnfcalloc(sizeof(ClockRange), 1);
+    clockRanges->next = NULL;
+    clockRanges->minClock = 10000;
+    clockRanges->maxClock = 165000;
+    clockRanges->clockIndex = -1;
+    clockRanges->interlaceAllowed = clockRanges->doubleScanAllowed = 0;
+    clockRanges->ClockMulFactor = clockRanges->ClockDivFactor = 1;
+    pScrn->progClock = TRUE;
+
+    if (0 >= xf86ValidateModes(pScrn, pScrn->monitor->Modes,
+			       pScrn->display->modes, clockRanges, linePitches,
+			       128, 2048, 128 * 4, 128, 2048,
+			       pScrn->display->virtualX,
+			       pScrn->display->virtualY,
+			       128 * 1024 * 1024, LOOKUP_BEST_REFRESH))
+	goto out;
+    xf86PruneDriverModes(pScrn);
+    pScrn->currentMode = pScrn->modes;
+    xf86PrintModes(pScrn);
+    xf86SetDpi(pScrn, 0, 0);
+
+    if (!xf86LoadSubModule(pScrn, "fb") ||
+	!xf86LoadSubModule(pScrn, "exa") ||
+	!xf86LoadSubModule(pScrn, "ramdac"))
+	goto out;
+
+    /* hate */
+    qxlUnmapMem(qxl, scrnIndex);
+
+    xf86DrvMsg(scrnIndex, X_INFO, "PreInit complete\n");
+    return TRUE;
+
+out:
+    if (clockRanges)
+	xfree(clockRanges);
+    if (qxl)
+	xfree(qxl);
+
+    return FALSE;
+}
+
+
+#ifdef XSERVER_LIBPCIACCESS
+enum qxl_class
+{
+    CHIP_QXL_1,
+};
+
+static const struct pci_id_match qxl_device_match[] = {
+    {
+	PCI_VENDOR_QUMRANET, PCI_CHIP_QXL_0100, PCI_MATCH_ANY, PCI_MATCH_ANY,
+	0x00030000, 0x00ffffff, CHIP_QXL_1
+    },
+
+    { 0 },
+};
+#endif
+
+static SymTabRec qxlChips[] =
+{
+    { PCI_CHIP_QXL_0100,	"QXL 1", },
+    { -1, NULL }
+};
+
+#ifndef XSERVER_LIBPCIACCESS
+static PciChipsets qxlPciChips[] =
+{
+    { PCI_CHIP_QXL_0100,    PCI_CHIP_QXL_0100,	RES_SHARED_VGA },
+    { -1, -1, RES_UNDEFINED }
+};
+#endif
+
+static void
+qxlIdentify(int flags)
+{
+    xf86PrintChipsets("qxl", "Driver for QXL virtual graphics", qxlChips);
+}
+
+static void
+qxlInitScrn(ScrnInfoPtr pScrn)
+{
+    pScrn->driverVersion    = 0;
+    pScrn->driverName	    = pScrn->name = "qxl";
+    pScrn->PreInit	    = qxlPreInit;
+    pScrn->ScreenInit	    = qxlScreenInit;
+    pScrn->EnterVT	    = qxlEnterVT;
+    pScrn->LeaveVT	    = qxlLeaveVT;
+}
+
+#ifndef XSERVER_LIBPCIACCESS
+static Bool
+qxlProbe(DriverPtr drv, int flags)
+{
+    int i, numUsed;
+    int numDevSections;
+    int *usedChips;
+    GDevPtr *devSections;
+
+    if ((numDevSections = xf86MatchDevice(qxl, &devSections)) <= 0)
+	return FALSE;
+
+    if (!xf86GetPciVideoInfo())
+	return FALSE;
+
+    numUsed = xf86MatchPciInstances(qxl, PCI_VENDOR_QUMRANET,
+				    qxlChips, qxlPciChips,
+				    devSections, numDevSections,
+				    drv, &usedChips);
+
+    xfree(devSections);
+
+    if (numUsed < 0) {
+	xfree(usedChips);
+	return FALSE;
+    }
+
+    if (flags & PROBE_DETECT) {
+	xfree(usedChips);
+	return TRUE;
+    }
+
+    for (i = 0; i < numUsed; i++) {
+	ScrnInfoPtr pScrn = NULL;
+	if ((pScrn = xf86ConfigPciEntity(pScrn, 0, usedChips[i], qxlPciChips,
+					 0, 0, 0, 0, 0)))
+	    qxlInitScrn(pScrn);
+    }
+
+    xfree(usedChips);
+    return TRUE;
+}
+
+#else /* pciaccess */
+
+static Bool
+qxlPciProbe(DriverPtr drv, int entity, struct pci_device *dev, intptr_t match)
+{
+    qxlScreen *qxl;
+    ScrnInfoPtr pScrn = xf86ConfigPciEntity(NULL, 0, entity, NULL, NULL,
+					    NULL, NULL, NULL, NULL);
+
+    if (!pScrn)
+	return FALSE;
+
+    if (!pScrn->driverPrivate)
+	pScrn->driverPrivate = xnfcalloc(sizeof(qxlScreen), 1);
+    qxl = pScrn->driverPrivate;
+    qxl->pci = dev;
+
+    qxlInitScrn(pScrn);
+
+    return TRUE;
+}
+
+#define qxlProbe NULL
+
+#endif
+
+static DriverRec qxl_driver = {
+    0,
+    "qxl",
+    qxlIdentify,
+    qxlProbe,
+    NULL,
+    NULL,
+    0,
+    NULL,
+#ifdef XSERVER_LIBPCIACCESS
+    qxl_device_match,
+    qxlPciProbe
+#endif
+};
+
+static pointer
+qxlSetup(pointer module, pointer opts, int *errmaj, int *errmin)
+{
+    static Bool loaded = FALSE;
+
+    if (!loaded) {
+	loaded = TRUE;
+	xf86AddDriver(&qxl_driver, module, 0);
+	return (void *)1;
+    } else {
+	if (errmaj)
+	    *errmaj = LDR_ONCEONLY;
+	return NULL;
+    }
+}
+
+static XF86ModuleVersionInfo qxlModuleInfo = {
+    "qxl",
+    MODULEVENDORSTRING,
+    MODINFOSTRING1,
+    MODINFOSTRING2,
+    XORG_VERSION_CURRENT,
+    0, 0, 0,
+    ABI_CLASS_VIDEODRV,
+    ABI_VIDEODRV_VERSION,
+    MOD_CLASS_VIDEODRV,
+    { 0, 0, 0, 0 }
+};
+
+_X_EXPORT XF86ModuleData qxlModuleData = {
+    &qxlModuleInfo,
+    qxlSetup,
+    NULL
+};
